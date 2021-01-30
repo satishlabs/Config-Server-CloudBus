6223f0b75de17ea93cf3df8ef1d65e90c90ed051
Satish Prasad <spd61185@gmail.com>
2021-01-30 18:44:57 +0530

    BookSearchMS updated

--- BookSearchMS/src/main/java/com/booksearchms/BookSearchMsApplication.java
+++ BookSearchMS/src/main/java/com/booksearchms/BookSearchMsApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-
 public class BookSearchMsApplication {
 
 	public static void main(String[] args) {
