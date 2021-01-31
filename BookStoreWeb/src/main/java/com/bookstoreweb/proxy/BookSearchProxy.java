f417509b37037df504ac340273c6f684479fcf69
Satish Prasad <spd61185@gmail.com>
2021-01-31 06:27:21 +0530

    BookSearchMS Updated

--- BookStoreWeb/src/main/java/com/bookstoreweb/proxy/BookSearchProxy.java
+++ BookStoreWeb/src/main/java/com/bookstoreweb/proxy/BookSearchProxy.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @FeignClient(name = "MyBookSearchMS")
 public interface BookSearchProxy {
 	
+	
 	@GetMapping("/allbooks")
 	public List<String> getAllBooks();
 	
